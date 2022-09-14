1434667248fc22b1acde952dcbdc3254fb6f5693
Max Cobb <maxxc@mac.com>
2022-09-14 11:11:14 +0100

    testing version 1.5.1-rc.1

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AgoraRtmKit",
-            url: "https://github.com/AgoraIO/AgoraRtm_macOS/releases/download/1.4.10/AgoraRtmKit.xcframework.zip",
-            checksum: "93960025bcd480ad860861f5cee8ef2264c27000327c50dcaca0fce7553c0519"
+            url: "https://github.com/AgoraIO/AgoraRtm_macOS/releases/download/1.5.1-rc.1/AgoraRtmKit.xcframework.zip",
+            checksum: "3c209b9ad1bbfab850a10edc2d8d0559aef0d07d171dd9b9c954542d8b8dd32d"
         )
 //        .binaryTarget(name: "AgoraRtmKit", path: "./AgoraRtmKit.xcframework")
     ]
